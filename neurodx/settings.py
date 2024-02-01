ae0ac3c766356e1ac65470c11a28b38451fffe88
Lorena Martínez Pineda <118283887+sloremart@users.noreply.github.com>
2024-02-01 09:46:06 -0500

    Ajustes en las rutas de los archivos

--- neurodx/settings.py
+++ neurodx/settings.py
@@ -148,7 +148,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL = '/media/'
+MEDIA_URL = '/media'
 ROOT_PATH_FILES_STORAGE = '/home/server'
 
 STATICFILES_DIRS = [

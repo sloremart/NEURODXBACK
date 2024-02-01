d17d1f79f06f800591e6a92cbaf9583861e4d4a0
Lorena Martínez Pineda <118283887+sloremart@users.noreply.github.com>
2024-02-01 09:56:41 -0500

    Ajustes rutas fix media url

--- gedocumental/views.py
+++ gedocumental/views.py
@@ -183,7 +183,7 @@ def donwloadFile(request, id_archivo):
 
         # Construir la ruta completa del archivo
         #archivo_path = os.path.join(settings.ROOT_PATH_FILES_STORAGE,'/',settings.MEDIA_ROOT, '/',str(archivo.RutaArchivo))
-        archivo_path = settings.ROOT_PATH_FILES_STORAGE + '/' + settings.MEDIA_ROOT + str(archivo.RutaArchivo);
+        archivo_path = settings.ROOT_PATH_FILES_STORAGE + settings.MEDIA_ROOT + str(archivo.RutaArchivo);
         print(archivo_path);
         # Verificar si el archivo realmente existe
         if not os.path.exists(archivo_path):

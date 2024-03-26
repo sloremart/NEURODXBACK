654e9c7104e2f28eddee0c6d81b32d8f669fed70
Lorena Martínez Pineda <118283887+sloremart@users.noreply.github.com>
2024-03-26 15:03:00 -0500

    ModuloT

--- neurodx/settings.py
+++ neurodx/settings.py
@@ -86,8 +86,8 @@ DATABASES = {
         'ENGINE': 'mysql.connector.django',
         'HOST': 'localhost',
         'PORT': '3306',
-        'USER': 'antares',
-        'PASSWORD': 'dic2401', 
+        'USER': 'root',
+        'PASSWORD': 'root', 
         'NAME': 'neurodx',
         'OPTIONS': {
             'autocommit': True,

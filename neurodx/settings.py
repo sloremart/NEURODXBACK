45dc8be545ee2e13892d94e08e38d51ba13789ad
Lorena Martínez Pineda <118283887+sloremart@users.noreply.github.com>
2024-05-21 17:06:19 -0500

    Update settings.py

--- neurodx/settings.py
+++ neurodx/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
 ]
 
 

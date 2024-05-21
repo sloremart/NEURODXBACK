60115fa9a2fa7df5f8583f58b06a3b9452f6fe0a
Lorena Martínez Pineda <118283887+sloremart@users.noreply.github.com>
2024-05-21 15:45:53 -0500

    Update settings.py

--- neurodx/settings.py
+++ neurodx/settings.py
@@ -36,7 +36,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'corsheaders',
     'rest_framework.authtoken',
     'neurodx',    
     'gedocumental',
